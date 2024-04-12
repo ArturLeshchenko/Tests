54b3cace3077273337752507984f30b322dfb21d
Artur Leshchenko <artur151199@mail.ru>
2024-04-12 12:28:36 +0300

    Аааа

--- src/Robot/Main.java
+++ src/Robot/Main.java
@@ -1,5 +1,6 @@
 package Robot;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,9 +10,6 @@ public class Main {
         System.out.println("Для начала игры нажмите 1");
         Scanner sc = new Scanner(System.in);
         String scnr = sc.nextLine();
-
-
-
 
         if (scnr.equals("1")) {
             System.out.println("Введите начальные координаты");
@@ -25,9 +23,9 @@ public class Main {
             Robot robot1 = new Robot(X1, Y1, Direction);
 
             if (Direction.equals("Up")){
-
                 if (X2 > X1) {
                     robot1.turnRight();
+                    Direction = "Right";
                     do {
                         X1 = X1 + 1;
                     }
@@ -35,6 +33,7 @@ public class Main {
 
                     if (Y2>Y1) {
                         robot1.turnLeft();
+                        Direction = "Up";
                         do {
                             Y1 = Y1 + 1;
                         }
@@ -42,21 +41,16 @@ public class Main {
                     }
                     else {
                         robot1.turnRight();
+                        Direction = "Down";
                         do {
                             Y1 = Y1 + 1;
                         }
                         while (Y1!=Y2);
                     }
                 }
-                else {
-                    robot1.turnLeft();
-                    do {
-                        X1 = X1 + 1;
-                    }
-                    while (X1 != X2);
-                }
                 if (X2 < X1) {
                     robot1.turnLeft();
+                    Direction = "Left";
                     do {
                         X1=X1 + 1;
                     }
@@ -64,6 +58,7 @@ public class Main {
 
                     if (Y2>Y1) {
                         robot1.turnRight();
+                        Direction = "Up";
                         do {
                             Y1=Y1+1;
                         }
@@ -71,38 +66,173 @@ public class Main {
                     }
                     else {
                         robot1.turnLeft();
+                        Direction = "Down";
                         do {
                             Y1 = Y1 + 1;
                         }
                         while (Y1!=Y2);
                     }
                 }
-                else {
+            }
+            if (Direction.equals("Down")){
+                if (X2 > X1) {
                     robot1.turnLeft();
+                    Direction = "Left";
                     do {
                         X1 = X1 + 1;
                     }
-                    while (X1!= X2);
+                    while (X1!=X2);
+
+                    if (Y2>Y1) {
+                        robot1.turnLeft();
+                        Direction = "Up";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                    else {
+                        robot1.turnRight();
+                        Direction = "Down";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
                 }
+                if (X2 < X1) {
+                    robot1.turnRight();
+                    Direction = "Right";
+                    do {
+                        X1=X1 + 1;
+                    }
+                    while (X1!=X2);
 
-            }
-            if (Direction.equals("Down")){
-
+                    if (Y2>Y1) {
+                        robot1.turnRight();
+                        Direction = "Up";
+                        do {
+                            Y1=Y1+1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                    else {
+                        robot1.turnLeft();
+                        Direction = "Down";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                }
             }
             if (Direction.equals("Right")){
+                if (X2 > X1) {
+                    do {
+                        X1 = X1 + 1;
+                    }
+                    while (X1!=X2);
 
+                    if (Y2>Y1) {
+                        robot1.turnLeft();
+                        Direction = "Up";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                    else {
+                        robot1.turnRight();
+                        Direction = "Down";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                }
+                if (X2 < X1) {
+                    robot1.turnLeft();
+                    robot1.turnLeft();
+                    Direction = "Left";
+                    do {
+                        X1=X1 + 1;
+                    }
+                    while (X1!=X2);
+
+                    if (Y2>Y1) {
+                        robot1.turnRight();
+                        Direction = "Up";
+                        do {
+                            Y1=Y1+1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                    else {
+                        robot1.turnLeft();
+                        Direction = "Down";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                }
             }
             if (Direction.equals("Left")){
+                if (X2 > X1) {
+                    robot1.turnRight();
+                    robot1.turnRight();
+                    Direction = "Right";
+                    do {
+                        X1 = X1 + 1;
+                    }
+                    while (X1!=X2);
 
+                    if (Y2>Y1) {
+                        robot1.turnLeft();
+                        Direction = "Up";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                    else {
+                        robot1.turnRight();
+                        Direction = "Down";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                }
+                if (X2 < X1) {
+                    do {
+                        X1=X1 + 1;
+                    }
+                    while (X1!=X2);
+
+                    if (Y2>Y1) {
+                        robot1.turnRight();
+                        Direction = "Up";
+                        do {
+                            Y1=Y1+1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                    else {
+                        robot1.turnLeft();
+                        Direction = "Down";
+                        do {
+                            Y1 = Y1 + 1;
+                        }
+                        while (Y1!=Y2);
+                    }
+                }
             }
+            Robot robot2 = new Robot(X1, Y1, Direction);
 
-
-            //Robot robot2 = new Robot(X1, Y1, Direction);
-
-
-            System.out.println(robot1.getX1());
-            System.out.println(robot1.getY1());
-            System.out.println(robot1.getDirection());
+            System.out.println(robot2.getX1());
+            System.out.println(robot2.getY1());
+            System.out.println(robot2.getDirection());
         }
     }
 }
